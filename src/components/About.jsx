9ab57264cb0dd2c3884840a8d601b9ae071977c7
saimyomyat <saimyomyat979@gmail.com>
2023-08-06 00:46:24 +0630

    all together

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,42 +19,48 @@ const About = () => {
   return (
     <div
       id="about"
-      className="  select-none  gap-4 flex md:flex-row flex-col-reverse justify-between items-center mt-10 w-[80%] mx-auto"
+      className="  select-none 
+       gap-4 flex md:flex-row flex-col-reverse justify-between items-center
+        mt-10 w-[80%] mx-auto"
     >
       <motion.div
         variants={fadeIn("right", 0.3)}
         initial="hidden"
         whileInView={"show"}
         viewport={{ once: false, amount: 0.7 }}
-        className=" font-semibold text-blue-800 dark:text-white mx-auto md:mx-0 w-[80%] md:w-[35%]"
+        className=" font-semibold  text-blue-800 dark:text-white mx-auto
+         md:mx-0 w-[80%] md:w-[50%]"
       >
-        <div className=" w-96 cstomcs flex justify-between">
-          <h1 className=" text-1xl">Name</h1>
-          <span className=" text-1xl">
-            {inView ? (
-              <TypeAnimation
-                sequence={["Sai Myo Myat", 200]}
-                wrapper="span"
-                speed={20}
-                repeat={Infinity}
-              />
-            ) : null}
-          </span>
-        </div>
-        <div className=" w-52 flex justify-between">
-          <h1 className=" text-1xl">Age</h1>
-          <span ref={ref} className=" text-1xl">
-            {inView ? <CountUp start={0} end={24} duration={3} /> : null}
+        <div className=" w-[80%] mx-auto md:mx-0">
+          <div className="    flex justify-between">
+            <h1 className=" text-1xl">Name</h1>
+            <span className=" text-1xl">
+              {inView ? (
+                <TypeAnimation
+                  sequence={["Sai Myo Myat", 200]}
+                  wrapper="span"
+                  speed={20}
+                  repeat={Infinity}
+                />
+              ) : null}
+            </span>
+          </div>
+          <div className="  flex justify-between">
+            <h1 className=" text-1xl">Age</h1>
+            <span ref={ref} className=" text-1xl">
+              {inView ? <CountUp start={0} end={24} duration={3} /> : null}
 
-            <span className=" ms-2">years old</span>
-          </span>
-        </div>
-        <div className=" flex justify-between">
-          <h1 className=" text-1xl">Position</h1>
-          <span className="  text-1xl">Frontend Developer</span>
-        </div>
-        <div className=' mt-3 text-sm '>
-          I have no experiences in this job.So,I want to serve as a junior developer. 
+              <span className=" ms-2">years old</span>
+            </span>
+          </div>
+          <div className=" flex justify-between">
+            <h1 className=" text-1xl">Position</h1>
+            <span className="  text-1xl">Frontend Developer</span>
+          </div>
+          <div className=" mt-3 text-sm ">
+            I have no experiences in this job.So,I want to serve as a junior
+            developer.
+          </div>
         </div>
       </motion.div>
 
@@ -63,7 +69,6 @@ const About = () => {
         initial="hidden"
         whileInView={"show"}
         viewport={{ once: false, amount: 0.7 }}
-       
         className="  duration-500 dark:bg-slate-500 bg-blue-400 cursor-pointer rounded-full p-5"
       >
         <img
